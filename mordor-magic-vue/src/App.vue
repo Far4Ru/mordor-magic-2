5cae42f754f725737a35722bc7b7dbfc0dfc2186
Sasha <faty422@mail.ru>
2021-09-16 22:38:29 +0300

    Шаблон главной страницы

--- mordor-magic-vue/src/App.vue
+++ mordor-magic-vue/src/App.vue
@@ -1,25 +1,113 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
     <v-app-bar
       app
-      color="primary"
-      dark
+      color="white"
+      flat
     >
-      <div class="d-flex align-center">
-        <h1 class="headline">Простой проект на Vue</h1>
-      </div>
+      <v-container class="py-0 fill-height">
+        <v-avatar color="grey">
+          <v-icon>
+            mdi-transmission-tower
+          </v-icon>
+        </v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+        <v-spacer></v-spacer>
+        <avatar-menu />
+      </v-container>
     </v-app-bar>
-    <v-main class="d-flex align-center text-center">
-      <router-view />
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item
+                  v-for="page in pages"
+                  :key="page"
+                  link
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ page }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  link
+                  color="grey lighten-4"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      Настройки
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+
+import AvatarMenu from '@/components/AvatarMenu'
 export default {
   name: 'App',
   data: () => ({
-    //
-  })
+    links: [
+      'Главная',
+      'Объявления',
+      'Участники',
+      'Библиотека',
+      'События'
+    ],
+    pages: [
+      'Профиль',
+      'Новости',
+      'Сообщения',
+      'Персонажи',
+      'Мои задачи',
+      'Закладки'
+    ]
+  }),
+  components: {
+    AvatarMenu
+  }
 }
 </script>
---
+++ mordor-magic-vue/src/components/AvatarMenu.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-menu
+    bottom
+    min-width="200px"
+    rounded
+    offset-y
+  >
+    <template v-slot:activator="{ on }">
+      <v-btn
+        icon
+        x-large
+        v-on="on"
+      >
+        <v-avatar
+          color="blue"
+          size="42"
+        >
+          <span class="white--text text-h5">{{ user.initials }}</span>
+        </v-avatar>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-list-item-content class="justify-center">
+        <div class="mx-auto text-center">
+          <v-avatar
+            color="blue"
+          >
+            <span class="white--text text-h5">{{ user.initials }}</span>
+          </v-avatar>
+          <h3>{{ user.fullName }}</h3>
+          <p class="text-caption mt-1">
+            {{ user.email }}
+          </p>
+          <v-divider class="my-3"></v-divider>
+          <v-btn
+            depressed
+            rounded
+            text
+          >
+            Настройки
+          </v-btn>
+          <v-divider class="my-3"></v-divider>
+          <v-btn
+            depressed
+            rounded
+            text
+          >
+            Выйти
+          </v-btn>
+        </div>
+      </v-list-item-content>
+    </v-card>
+  </v-menu>
+</template>
+<script>
+export default {
+  name: 'AvatarMenu',
+  data: () => ({
+    user: {
+      initials: 'S',
+      fullName: 'Sasha',
+      email: 'far4ru@gmail.com'
+    }
+  })
+}
+</script>
